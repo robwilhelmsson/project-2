fad6821e7c173ec63fbd5f40c6385c6cd235a942
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2022-11-15 17:30:35 +0100

    figuring out random country return

--- src/components/CountryShow.tsx
+++ src/components/CountryShow.tsx
@@ -23,41 +23,64 @@ type Country = Array<ICountries>
 
 
 function CountryShow() {
-  const [countries, setCountries] = React.useState<Country>([])
 
-  function randomCountry() {
-    const count = Math.floor(Math.random() * 250)
-    return countryCodes[count]
-  }
+  const [countries, setCountries] = React.useState<Country>([])
+  const [randomCountry, setRandomCountry] = React.useState<Country>([])
+
 
   async function fetchCountry() {
-    const resp = await fetch(`https://restcountries.com/v3.1/alpha/${randomCountry()}`)
+    const resp = await fetch(`https://restcountries.com/v3.1/all`)
     const countries = await resp.json()
     setCountries(countries)
-    console.log(countries)
+    setRandomCountry(countries[Math.floor(Math.random() * countries.length)])
   }
 
   useEffect(() => {
     fetchCountry()
   }, [])
 
+  console.log(randomCountry)
+
+  // function randomCountry() {
+  //   const randomCountries = setRandomCountries(Math.floor(Math.random() * countries.length))
+  //   setRandomCountries[0](randomCountries)
+  // }
+  // console.log(randomCountry())
+
+  // const country = countries[Math.floor(Math.random() * countries.length)]
+  // console.log(country)
+
   return (
-    <section className="section">
-      <div className="container">
+    <section className="section">     
+
+    <div>
+      {/* <h2>{randomCountry}</h2> */}
+    </div>
+
+      {/* <div>
         {countries.map((country) => {
-          return <div key={country.flags.png}>
-            <p>{country.name.common}</p>
+          return <div key={country.name.common}>
+            <h2>{country.name.common}</h2>
             <img src={country.flags.png} alt={country.name.common} />
           </div>
+        })
+        }
+      </div> */}
+
+      {/* <div className="container">
+        {countries.map((country) => {
+          return <div key={country.name.common}>
+            <button onClick={fetchCountry}>{country.name.common}</button>
+            <button onClick={fetchCountry}>{country.name.common}</button>
+            <button onClick={fetchCountry}>{country.name.common}</button>
+            <button onClick={fetchCountry}>{country.name.common}</button>
+            </div> 
         })}
-        <input placeholder="Answer here" type="text" />
-      </div>
+      </div> */}
+
     </section>
   )
 
 }
-
-
-
 
 export default CountryShow
